2d86dd5d5318c4ad7a7447288ad0884bb00a57ed
Steven Lu <tacticalazn@gmail.com>
2015-01-11 00:20:21 -0500

    ability to accept device keys

--- routes/spark/index.js
+++ routes/spark/index.js
@@ -2,5 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.use('/oauth', require('./oauth'));
+router.use('/v1/provisioning', require('./provisioning'));
 
 module.exports = router;
---
+++ routes/spark/provisioning.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var models = require('../../lib/models');
+var async = require('async');
+var deviceKeys = require('../../lib/device_keys');
+
+router.post('/:id', function(req, res, next) {
+  var deviceId = req.body.deviceID;
+  var pubKey = req.body.publicKey;
+
+  async.parallel([
+    function(cb) {
+      var device = new models.Device({
+        device_id: deviceId,
+        device_key: pubKey
+      });
+
+      device.save(cb);
+    },
+    function(cb) {
+      deviceKeys.save(deviceId, pubKey, cb);
+    }
+  ], function(err) {
+    if (err) return next(err);
+    return cb();
+  });
+
+});
+
+module.exports = router;
