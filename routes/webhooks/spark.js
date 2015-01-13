1aab288190ae0249d5952f19526ccfa65337cd68
Steven Lu <tacticalazn@gmail.com>
2015-01-13 15:03:38 -0500

    adding logging

--- routes/webhooks/spark.js
+++ routes/webhooks/spark.js
@@ -2,18 +2,26 @@ var express = require('express');
 var router = express.Router();
 var spark = require('../../lib/spark');
 var plugs = require('../../plugs');
+var winston = require('../../lib/winston');
 
 router.post('/', function(req, res, next) {
   var evt = req.body.name;
   var coreId = req.body.coreid;
   var data = req.body.data;
+  var obj = {
+    evt: evt,
+    coreId: coreId,
+    data: data
+  }
 
   if (!plugs.map[evt]) {
+    winston.warn('event was received but unhandled', obj);
     return res.send(404);
   }
 
   plugs.map[evt](coreId, data, function(err) {
     if (err) return next(err);
+    winston.info('event received', obj);
     res.send();
   });
 });
