aee2d953dbe93d307836756b6977f8f5ec51ca7b
Steven Lu <tacticalazn@gmail.com>
2015-01-14 00:34:05 -0500

    storing when we last see a device

--- plugs/spark/status.js
+++ plugs/spark/status.js
@@ -1,9 +1,33 @@
 var jobs = require('../../lib/jobs');
+var async = require('async');
+var models = require('../../lib/models');
 
 module.exports = function(id, data, cb) {
-  if (data == 'online') {
-    jobs.create('update_device_firmware', {
-      spark_id: id
-    }).save(cb);
-  }
+  async.parallel([
+    function(cb) {
+      if (data === 'online') {
+        jobs.create('update_device_firmware', {
+          spark_id: id
+        }).save(cb);
+      } else {
+        cb();
+      }
+    },
+    function(cb) {
+      models.Device.find({
+        spark_id: id
+      }).exec(function(err, device) {
+        if (err) return cb(err);
+
+        var connected = false;
+        if (data === 'online') {
+          connected = true;
+        }
+
+        device.updateConnectionStatus(connected, cb);
+      })
+    }
+  ], cb);
+
+
 }
