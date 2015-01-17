caa821f86eef232755854a37e5ac39324420d545
Steven Lu <tacticalazn@gmail.com>
2015-01-17 00:38:18 -0500

    fixing hook about being in bed

--- plugs/alarm/is_in_bed.js
+++ plugs/alarm/is_in_bed.js
@@ -2,9 +2,9 @@ var models = require('../../lib/models');
 
 module.exports = function(id, data, cb) {
 
-  var eventName = 'got_in_bed';
-  if (data == false) {
-    eventName = 'got_out_of_bed';
+  var eventName = 'in_bed';
+  if (data == "false") {
+    eventName = 'out_of_bed';
   }
 
   models.Device.findOne({
