a9d2296be26d878058ae48d9ab799f2805d64b16
Steven Lu <tacticalazn@gmail.com>
2015-01-22 01:19:50 -0500

    update variables when spark is connected

--- plugs/spark/status.js
+++ plugs/spark/status.js
@@ -3,31 +3,40 @@ var async = require('async');
 var models = require('../../lib/models');
 
 module.exports = function(id, data, cb) {
-  async.parallel([
-    function(cb) {
-      if (data === 'online') {
-        jobs.create('update_device_firmware', {
-          spark_id: id
-        }).save(cb);
-      } else {
-        cb();
+
+  models.Device.findOne({
+    spark_id: id
+  }).exec(function(err, device) {
+    if (err) return cb(err);
+
+    var funcs = [
+      function(cb) {
+        device.updateConnectionStatus(data === 'online', cb);
       }
-    },
-    function(cb) {
-      models.Device.findOne({
-        spark_id: id
-      }).exec(function(err, device) {
-        if (err) return cb(err);
+    ];
 
-        var connected = false;
-        if (data === 'online') {
-          connected = true;
+    device.isFirmwareCurrent(function(err, current) {
+      if (err) return cb(err);
+
+      if (data === 'online') {
+        if (!current) {
+          funcs.push(function(cb) {
+            jobs.create('update_device_firmware', {
+              spark_id: id
+            }).save(cb);
+          });
+        } else {
+          funcs.push(function(cb) {
+            jobs.create('sync_devices', {
+              device_id: device._id
+            }).save(cb);
+          });
         }
+      }
 
-        device.updateConnectionStatus(connected, cb);
-      })
-    }
-  ], cb);
+      async.parallel(funcs, cb);
+    });
 
+  });
 
 }
